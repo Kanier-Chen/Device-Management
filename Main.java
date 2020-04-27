189e02b643a310a1c3464f67cd7ba7c209c580d2
YR-Creator <64390935+YR-Creator@users.noreply.github.com>
2020-04-27 11:56:13 +0800

    Add files via upload
    大数阶乘的主程序和实现功能函数

---
+++ Main.java
@@ -0,0 +1,13 @@
+package test½×³Ë;
+
+import java.math.BigInteger;
+
+
+public class Main{
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GUI gui = new GUI();
+	}
+
+}
